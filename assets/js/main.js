087e7db4d1d7770e5b6ec9a0c767777b5becf066
Abel <abel_borges@yahoo.com.br>
2022-11-04 01:39:13 -0300

    Incluído arquivo javascript e montagem de cards dinâmicos

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+function createGame(player1, hour, player2) {
+  return `
+  <li>
+    <img src="./assets/flags/icon=${player1}.svg" alt="bandeira do ${player1}" />
+    <strong>${hour}</strong>
+    <img src="./assets/flags/icon=${player2}.svg" alt="bandeira da ${player2}" />
+  </li>
+  `
+}
+
+function createCard(date, day, games) {
+  return `
+      <div class="card">
+          <h2>${date} <span>${day}</span></h2>
+          <ul>
+            ${games}
+          </ul>
+        </div>
+  `
+}
+
+document.querySelector("#app").innerHTML = `
+      <header>
+        <img src="./assets/logo.svg" alt="Logo da NLW" />
+      </header>
+      <main id="cards">
+        ${createCard(
+          "24/11",
+          "quinta",
+          createGame("switzerland", "07:00", "cameroon") +
+            createGame("uruguay", "10:00", "south korea") +
+            createGame("portugal", "13:00", "ghana") +
+            createGame("brazil", "16:00", "serbia")
+        )}
+        ${createCard(
+          "28/11",
+          "segunda",
+          createGame("cameroon", "07:00", "serbia") +
+            createGame("south korea", "10:00", "ghana") +
+            createGame("brazil", "13:00", "switzerland") +
+            createGame("portugal", "16:00", "uruguay")
+        )}
+        ${createCard(
+          "02/12",
+          "sexta",
+          createGame("ghana", "12:00", "uruguay") +
+            createGame("south korea", "12:00", "portugal") +
+            createGame("serbia", "16:00", "switzerland") +
+            createGame("cameroon", "16:00", "brazil")
+        )}
+      </main>
+`
